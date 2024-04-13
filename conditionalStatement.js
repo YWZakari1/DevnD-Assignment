a7aee9eca54e4e20cdbf1c8a4e75af5bbfe3a2c4
Yusuf Zakari <yzakari@gmail.com>
2024-04-13 13:47:06 -0500

    firs main version

---
+++ conditionalStatement.js
@@ -0,0 +1,154 @@
+//1. Age Eligibility Checker: Determine if a person is eligible to vote based on their age.
+
+// let voteAge = 18
+// let votingAge = Number(window.prompt())
+
+// if(votingAge >= voteAge){
+//     console.log("you are " + `${votingAge}` + "yrs of Age, You can Vote")
+// }else if(votingAge <= voteAge){
+//         console.log("Your are " + `${votingAge}` + "yrs of Age, You can't vote")
+//     }
+
+//2. . Even or Odd Number Identifier: Check if a given number is even or odd and display a message.
+
+// let numChecker = Number(window.prompt());
+
+// function numberChecker(x){
+//     if(x % 2 === 0){
+//         console.log("Number " + `${x}` + " is an Even Number")
+//         return x
+//     }else if(x % 2 === 1){
+//         console.log("Number " + `${x}` + " is an Odd Number")
+//         return x
+//     }
+// }
+
+// numberChecker(numChecker)
+
+// if (numChecker % 2 === 0) {
+//   console.log("Number " + `${numChecker}` + " is an Even Number");
+// } else if (numChecker % 2 === 1) {
+//   console.log("Number " + `${numChecker}` + " is an Odd Number");
+// }
+
+// // Vowel or Consonant Classifier: Determine if a given letter is a vowel or a consonant.
+
+// let checkVC = window.prompt()
+
+// function checkVowelAndConsonate(vc){
+//     if(vc === "a" || vc ==="e" || vc ==="i" || vc ==="o" || vc ==="u"){
+//         console.log(`${vc}` + " is a Vowel ")
+//         return vc
+//     }else if(vc === vc.Number()){
+//         console.log("Please enter a vowel or consonant")
+//         return vc
+//     }else{
+//         console.log(`${vc}` + " This is a consonant")
+//         return vc
+//     }
+// }
+
+// checkVowelAndConsonate(checkVC)
+
+// 4. Largest Number Finder: Compare two numbers and return the larger one.
+
+// let numA = Number(window.prompt())
+// let numB = Number(window.prompt())
+
+// if(numA > numB){
+//     console.log(numA + " is greater than " + numB)
+// }else if(numA < numB){
+//     console.log(numA + " is less than " + numB)
+// }
+
+//Simple Calculator: Create a calculator that can perform basic arithmetic operations (+, -, *, /).
+
+// let operator = window.prompt("Enter Mathematical operator:")
+// let num1 = Number(window.prompt("Enter Number 1:"))
+// let num2 = Number(window.prompt("Enter Number 2:"))
+// let calResult = 0
+
+// if(operator ==="*"){
+//     calResult = num1 * num2
+//     console.log(num1 + " * " + num2 + " is = " + calResult)
+// }else if(operator ==="+"){
+//     calResult = num1 + num2
+//     console.log(num1 + " + " + num2 + " is =  " + calResult)
+// }else if(operator ==="-"){
+//     calResult = num1 - num2
+//     console.log(num1 + " - " + num2 + " is =  " + calResult)
+// }else if(operator ==="/"){
+//     calResult = num1 / num2
+//     console.log(num1 + " / " + num2 + " is =  " + calResult)
+// }else{
+//     console.log(" please ensure the operator is correct ")
+// }
+
+//Switch statement
+
+// let votingAge = Number(window.prompt("Please enter your Age: "));
+// let dayOfTheWeek = window.prompt("Please enter your Age: ");
+
+
+// switch (dayOfTheWeek) {
+//   case 1:
+//     console.log("Today is Monday");
+//     break;
+
+//   case 2:
+//     console.log("Today is Tuesday");
+//     break;
+
+//   case 3:
+//     console.log("Today is Wednesday");
+//     break;
+
+//   case 4:
+//     console.log("Today is Thursday");
+//     break;
+
+//   case `5`:
+//     console.log("Today is Friday");
+//     break;
+
+//   case `6`:
+//     console.log("Today is Saturday");
+//     break;
+
+//   case `7`:
+//     console.log("Today is Sunday");
+//     break;
+
+//   default:
+//     console.log("Please Enter a Correct day of the week");
+//     break;
+// }
+
+
+// let gradeRecieved = Number(window.prompt("Enter Score gained:"));
+
+// switch(gradeRecieved){
+//     case 90:
+//         console.log("You Scored 90 and got the grade A");
+//         break;
+
+//     case 80:
+//         console.log("You scored 80 and got the grade B");
+//         break;
+    
+//     case 70:
+//         console.log("You scored 70 and got the grade C");
+//         break;
+    
+//     case 60:
+//         console.log("You scored 60 and got the grade D");
+//         break;
+    
+//     case 50:
+//         console.log("You scored 50 and got the grade F");
+//         break;
+
+//     default:
+//         console.log("please enter the right Score")
+// }
+
